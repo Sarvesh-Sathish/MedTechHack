e2b39a685a7330556883cd9f7a50edbfbbd0d818
wertoto <59776067+wertoto@users.noreply.github.com>
2020-12-19 20:21:54 -0600

    pp

--- app/src/main/java/com/example/medtechhack/MainActivity.kt
+++ app/src/main/java/com/example/medtechhack/MainActivity.kt
@@ -4,9 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    // pp f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
+
+// Main idea:
+// Your very own personal dermatologist
+// WHY? Individuals are hesitant to book appointments with their skin doctors, often for conditions which may be dangerous yet easily diagnosable
+// also people may not always have ready access to visit their dermatologists.
+// SOLUTION: A personal dermatology app for quick diagnosis and guidance will take away the initial barriers to take initative in skin care
+// the app will: offer diagnostics, keep track of current medications, offer suggestions
+// TECHNOLOGY APPLICATION: Coded through app UI, application of ML and computer vision
+// BUSINESS ASPECT: A tangible product, meaning that the app is very capable of being applied into practice today without the barriers of needed
+// research and etc. A subscription service will allow users to access user data stats and skincare plans/ products as well as expert analysis.
